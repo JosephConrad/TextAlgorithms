1e9831f6e4c2bf334c3cde84abd2b7fda3d1f17f
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-02-15 00:28:24 +0100

    Text: fix prefix suffix

--- prefixSuffix.py
+++ prefixSuffix.py
@@ -6,20 +6,20 @@
 
 
 def prefixSufix(x):
-    m = len(x)+1
+    # ilosc znakow w tekscie
+    m = len(x)
     
     P=[-1]
     t=-1
-    for j in range(1, m):
-        while t>=0 and x[t+1] != x[j]:
+    for j in range(0, m):
+        while t>=0 and x[t] != x[j]:
             t=P[t]
         t+=1
-        print m
         P.append(t) 
     print P
     return P
     
 
-#x = "01101001100101101001011001101001"
-x = "0abababababb"
+x = "01101001100101101001011001101001"
+#x = "abababababb"
 print prefixSufix(x)
