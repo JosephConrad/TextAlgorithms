6126b0f355001ca2cdcb34f3b6214af1f41b607c
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-02-15 19:27:09 +0100

    Text: cleanup

--- mp.py
+++ mp.py
@@ -1,26 +1,21 @@
-
 from prefixSuffix import *
 
 
 def morissPratt(text, pattern):
     n = len(text)
-    x = pattern  
     m = len(pattern)
-    y = text
-    i = 0
-    j = 0 
+    i, j = 0, 0
     P = prefixSufix(pattern)
     while i <= n - m:
         # dopoki indeks j jest mniejszy od dlugosci wzorca
         while j < m and pattern[j] == text[i+j]:
             j += 1
         
-        if j == m: 
-            print P
-            print j
-            return True
+        if j == m: return True
+        
         i = i + j - P[j]
         j = max(0, P[j])
+    
     return False
 
 
